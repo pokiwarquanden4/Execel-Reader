b992ac76406c885893ae7bf28a82e6435c01c9db
Quang <pokiwarquanden4@gmail.com>
2023-03-31 21:03:04 +0700

    update

--- src/main/java/com/example/excelreader/Controller/TestController.java
+++ src/main/java/com/example/excelreader/Controller/TestController.java
@@ -5,7 +5,9 @@ import com.example.excelreader.Service.ExportExcel.ExportExcel;
 import com.example.excelreader.Service.ReadExcel.ReadExcelDTB;
 import com.example.excelreader.Service.ReadExcel.ReadExcelFileService;
 import com.example.excelreader.Model.ResponseData;
+import com.example.excelreader.entity.Template;
 import com.example.excelreader.repository.ImeisRepo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,7 +27,9 @@ public class TestController {
     @Autowired
     ImeisRepo imeisRepo;
     @PostMapping("/excelReader")
-    public String excelReader(@RequestParam("file") MultipartFile file) throws IOException {
+    public String excelReader(@RequestParam("file") MultipartFile file, @RequestParam("template") String template) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Template object = mapper.readValue(template, Template.class);
         readExcelDTB.writeToDB(readExcelFileService.readExcelFile(file));
         return "OK";
     }
